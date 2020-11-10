f2a23eb36ef07c02c7075e78ffe62ac7f595f784
luketeam5 <40070133+luketeam5@users.noreply.github.com>
2020-11-10 17:10:54 +0100

    Added extra description to code

--- ScriptDescription.lua
+++ ScriptDescription.lua
@@ -1,16 +1,20 @@
-local PlayerId = plugin:GetStudioUserId()
-local studioname = game.Players:GetNameFromUserIdAsync(PlayerId)
-local timestamp = os.time()
-local format = "%d.%m. %Y  %H:%M"
-local dateDay = os.date(format, timestamp)
-local toolbar = plugin:CreateToolbar("Script Description")
-local server = toolbar:CreateButton("Server script", "Create server script", "rbxassetid://4458901886")
-local locals = toolbar:CreateButton("Local script", "Create local script", "rbxassetid://5939819076")
-local module = toolbar:CreateButton("Module script", "Create module script", "rbxassetid://5939818869")
+-- I understand this code is terrible but i was in hurry making this maybe i will update and fix it one day
 
-local function onButton1Clicked()
-	local newScript = Instance.new("Script")
-	newScript.Parent = game:GetService("Workspace")
+
+local PlayerId = plugin:GetStudioUserId() -- Gets ID of user in studio
+local studioname = game.Players:GetNameFromUserIdAsync(PlayerId) -- Gets player name from ID
+local timestamp = os.time() -- Stores timestamp
+local format = "%d.%m. %Y  %H:%M" -- Makes format for date creation (%d day / %m month / %Y year / %H hour / %M minute)
+local dateDay = os.date(format, timestamp) -- Created date from timestamp and format
+local toolbar = plugin:CreateToolbar("Script Description") -- Creates toolbar for buttons
+local server = toolbar:CreateButton("Server script", "Create server script", "rbxassetid://4458901886") -- Makes buttons
+local locals = toolbar:CreateButton("Local script", "Create local script", "rbxassetid://5939819076") -- TODO: Upload high quality version of Local Script icon
+local module = toolbar:CreateButton("Module script", "Create module script", "rbxassetid://5939818869") -- TODO: Upload high quality version of Module Script icon
+
+local function onButton1Clicked() 
+	local newScript = Instance.new("Script") -- Creates script
+	newScript.Parent = game:GetService("Workspace") -- Sets script parent
+	-- "\" at end of lines adds new line to script. It works just format colors are broken..
 	newScript.Source = "--[[ ---------- Info ---------- \
 	WRITER(S): " .. studioname .. "  \
 	Creation date: " .. dateDay .. "\
@@ -18,7 +22,7 @@ local function onButton1Clicked()
 	Decription:  \
 --]] ---------- Info ----------"
 end
-server.Click:Connect(onButton1Clicked)
+server.Click:Connect(onButton1Clicked) -- Calls function
 
 local function onButton2Clicked()
 	local newScript = Instance.new("LocalScript")
